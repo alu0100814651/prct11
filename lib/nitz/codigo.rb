10872e18b62ab91755059b3ec79b6b07855ccf8f
alu0100814651 <alu0100814651@ull.edu.es>
2015-11-04 16:22:42 +0000

    Creando la prueba aceptada del titulo

--- lib/nitz/codigo.rb
+++ lib/nitz/codigo.rb
@@ -1,6 +1,7 @@
 class Biblio
-    attr_reader :autor
-    def initialize (x)
+    attr_reader :autor, :titulo
+    def initialize (x, y)
         @autor = x
+        @titulo = y
     end
 end
--- spec/codigo_pruebas.rb
+++ spec/codigo_pruebas.rb
@@ -3,12 +3,15 @@ require 'nitz/codigo'
 
 describe Biblio do
     before :each do
-        @obj1 = Biblio.new('Dave Thomas')
+        @obj1 = Biblio.new('Dave Thomas', 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide')
     end
     
     describe "Primer Conjunto de Pruebas" do
         it "Tiene que existir uno o mas autores" do
             @obj1.autor.should eq('Dave Thomas')
+        end
+        it "Tiene que existir un titulo" do
+            @obj1.titulo.should eq('Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide')
         end
     end
 end
