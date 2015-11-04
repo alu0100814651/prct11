b16731e6a3d6667a720af729bfaec6d73a7747fd
alu0100814651 <alu0100814651@ull.edu.es>
2015-11-04 16:39:27 +0000

    Creando la prueba de la edicion con fallos

--- spec/codigo_pruebas.rb
+++ spec/codigo_pruebas.rb
@@ -3,7 +3,7 @@ require 'nitz/codigo'
 
 describe Biblio do
     before :each do
-        @obj1 = Biblio.new('Dave Thomas', 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', '(The Facets of Ruby)', 'Pragmatic Bookshelf;')
+        @obj1 = Biblio.new('Dave Thomas', 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', '(The Facets of Ruby)', 'Pragmatic Bookshelf;', '4')
     end
     
     describe "Primer Conjunto de Pruebas" do
@@ -19,5 +19,7 @@ describe Biblio do
         it "Tiene que existir una editorial" do
             @obj1.editorial.should eq('Pragmatic Bookshelf;')
         end
+        it "Tiene que existir un numero de edicion" do
+            @obj1.edicion.should eq('4')
     end
 end
