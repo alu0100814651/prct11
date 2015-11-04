58b5837163ab9b9ddfeec417c962d89bc7709f5e
alu0100814651 <alu0100814651@ull.edu.es>
2015-11-04 15:40:38 +0000

    haciendo la primera prueba con fallos

--- spec/codigo_pruebas.rb
+++ spec/codigo_pruebas.rb
@@ -1,2 +1,13 @@
 require 'spec_helper'
 require 'niz/codigo'
+
+describe Biblio do
+    before :each do
+        @obj1 = Biblio.new['Dave Thomas']
+    end
+    
+    describe "Primer Conjunto de Pruebas" do
+        it "Tiene que existir uno o mas de un autores" do
+        @obj1.autor.should eq(['Dave Thomas'])
+    end
+end
