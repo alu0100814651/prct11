618975daa6855c4b302d198b119fe4b2f806ada1
alu0100814651 <alu0100814651@ull.edu.es>
2015-11-12 11:27:43 +0000

    poniendo todas las pruebas aprovadas

--- spec/codigo_pruebas.rb
+++ spec/codigo_pruebas.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'nitz/codigo'
+require 'nitz/libreria_cola'
 
 describe Biblio do
     before :each do
@@ -64,66 +65,70 @@ end
     before :all do
         
         #Ejemplos 
-                             
-        @bib1 = Biblio.new(['Dave Thomas','Andy Hunt','Chad Fowler'], 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', 'Pragmatic Bookshelf', 4, 'July 7 2013', ['9781937785499', '1937785491'], 'The Facets of Ruby')
-        @bib2 = Biblio.new('Scott Chacon', 'Pro Git 2009th Edition', 'Apress', 2009, 'August 27 2009', ['9781430218333','1430218339'], 'Pro')
-        @bib3 = Biblio.new(['David Flanagan','Yukihiro Matsumoto'], 'The Ruby Programming Language', 'O’Reilly Media', 1, 'February 4 2008', ['0596516177','9780596516178'])
-        @bib4 = Biblio.new(['David Chelimsky','Dave Astels','Bryan Helmkamp','Dan North','Zach Dennis','Aslak Hellesoy'], 'The RSpecBook: Behaviour Driven Development with RSpec, Cucumber, and Friends', 'Pragmatic Bookshelf', 1, 'December 25 2010', ['1934356379','9781934356371'], 'The Facets of Ruby')
-        @bib5 = Biblio.new('Richard E. Silverman','Git Pocket Guide', 'O’Reilly Media', 1, 'August 2 2013', ['1449325866','9781449325862'])
+        @bib1 = Biblio.new('Dave Thomas', 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', 'The Facets of Ruby', 'Pragmatic Bookshelf', '4', 'July 7 2013', ['9781937785499', '1937785491'])
+        @bib2 = Biblio.new('Scott Chacon', 'Pro Git 2009th Edition', 'Pro', 'Apress', '2009', 'August 27 2009', ['9781430218333','1430218339'])
+        @bib3 = Biblio.new('David Flanagan', 'The Ruby', 'Programming Language', 'O’Reilly Media', '1', 'February 4 2008', ['0596516177','9780596516178'])
+        @bib4 = Biblio.new('David Chelimsky', 'The RSpecBook: Behaviour Driven Development with RSpec, Cucumber, and Friends', 'The Facets of Ruby', 'Pragmatic Bookshelf', '1', 'December 25 2010', ['1934356379','9781934356371'])
+        @bib5 = Biblio.new('Richard E. Silverman','Git', 'Pocket Guide', 'O’Reilly Media', '1', 'August 2 2013', ['1449325866','9781449325862'])
         
         @cola1 = Cola.new()
-        
+        @cola2 = Cola.new()
+        @cola3 = Cola.new()
     end
+    
     describe "Creamos una cola" do
         it "El nodo inicial es nil" do
-            expect(@cola1.principio).to eq(nil)
+            expect(@cola1.start).to eq(nil)
         end
         it "El nodo final tmb es nil" do
-            expect(@cola1.fin).to eq(nil)
-            #@c1.fin.should eq(nil)
+            expect(@cola1.finish).to eq(nil)
         end
     end
+    
     describe "Vemos si la cola está vacía" do
         it "Debe existir el método 'vacía'" do
-            expect(@cola1.vacia).to eq(true)
+            expect(@cola2.vacia).to eq(true)
         end
     end
+    
     describe "Insertar elemento en cola" do
         it "Debe existir este método" do
-            expect(@cola1.insertar(@bib1)).to eq(true)
+            expect(@cola2.insertar(@bib1)).to eq(true)
         end
     end
+    
     describe "Extraer de la cola" do
         it "Debe existir este método" do
-            expect(@cola1.insertar(@bib1)).to eq(true)
-            expect(@cola1.extraer).to eq(@bib1)
+            expect(@cola2.insertar(@bib1)).to eq(true)
+            expect(@cola2.extraer).to eq(@bib1)
         end
     end
+    
     describe "Insertar mas de un elemento en la cola" do
         it "Podemos insertar mas de un elemento en la cola" do
             
-            expect(@cola1.insertar(@bib1)).to eq(true)
-            expect(@cola1.insertar(@bib2)).to eq(true)
-            expect(@cola1.insertar(@bib3)).to eq(true)
-            expect(@cola1.insertar(@bib4)).to eq(true)
-            expect(@cola1.insertar(@bib5)).to eq(true)
+            expect(@cola3.insertar(@bib1)).to eq(true)
+            expect(@cola3.insertar(@bib2)).to eq(true)
+            expect(@cola3.insertar(@bib3)).to eq(true)
+            expect(@cola3.insertar(@bib4)).to eq(true)
+            expect(@cola3.insertar(@bib5)).to eq(true)
         end
     end
+    
     describe "Se deberia poder extraer mas de un elemento de la cola" do
         it "Podemos extraer mas de un elemento de la cola" do
             
-            expect(@cola1.insertar(@bib1)).to eq(true)
-            expect(@cola1.insertar(@bib2)).to eq(true)
-            expect(@cola1.insertar(@bib3)).to eq(true)
-            expect(@cola1.insertar(@bib4)).to eq(true)
-            expect(@cola1.insertar(@bib5)).to eq(true)
+            expect(@cola3.insertar(@bib1)).to eq(true)
+            expect(@cola3.insertar(@bib2)).to eq(true)
+            expect(@cola3.insertar(@bib3)).to eq(true)
+            expect(@cola3.insertar(@bib4)).to eq(true)
+            expect(@cola3.insertar(@bib5)).to eq(true)
             
-            expect(@cola1.extraer).to eq(@bib1)
-            expect(@cola1.extraer).to eq(@bib2)
-            expect(@cola1.extraer).to eq(@bib3)
-            expect(@cola1.extraer).to eq(@bib4)
-            expect(@cola1.extraer).to eq(@bib5)
+            expect(@cola3.extraer).to eq(@bib1)
+            expect(@cola3.extraer).to eq(@bib2)
+            expect(@cola3.extraer).to eq(@bib3)
+            expect(@cola3.extraer).to eq(@bib4)
+            expect(@cola3.extraer).to eq(@bib5)
         end
     end
-            
 end
