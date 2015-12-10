3de822e8180214298e7523bed1531ec71848fdbc
alu0100814651 <alu0100814651@ull.edu.es>
2015-12-10 10:05:33 +0000

    Añadiendo más pruebas (con fallos)

--- spec/codigo_spec.rb
+++ spec/codigo_spec.rb
@@ -8,9 +8,9 @@ describe APA do
          @biblio1 = Biblio.new(['Dave', 'Andy'], ['Thomas', 'Hunt'], 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', 'Pragmatic Bookshelf', 4, 'July 7 2013', ['9781937785499', '1937785491'], 'The Facets of Ruby')
          @biblio2 = Biblio.new(['Nitesh'],['Gul'],'Cloud9', 'LPP', 1404, 'December 9 2015', ['9781937785499', '1937785491'], 'The Facets of Ruby')
          @biblio3 = Biblio.new(['Scott'],['Chacon'], 'Pro Git 2009th Edition', 'Apress', 2009, 'August 27 2000', ['9781430218333','1430218339'], 'Pro')
-         @biblio4 = Biblio.new(['Scott'],['Chacon'], 'Abecedario', 'Apress', 2009, 'August 27 2000', ['9781430218333','1430218339'], 'Pro')
-         @revista1 = Revista.new(['Dave', 'Andy'], ['Thomas', 'Hunt'], 'programming ruby', 'Pragmatic Bookshelf', 4, 'July 7 2013', ['9781937785499', '1937785491'], 'The Facets of Ruby')
-         @periodico1 = Periodico.new(['Maria'], ['Porr'],'Noticias', 'Diario de avisos', 1, ['9781937785499', '1937785491'], 'February 10 2013', 15)
+         @biblio4 = Biblio.new(['Scott'],['Chacon'], 'Apple', 'Antigeno', 2009, 'August 27 2000', ['9781430218333','1430218339'], 'Pro')
+         @revista1 = Revista.new(['David'],['Flanagan'], 'Revista Prueba', 'Probando', 1, '0596586177', 'December 15 2015', 1)
+         @periodico1 = Periodico.new(['Maria'], ['Rgez'], 'noticias', 'Avisos', 105, ['9781937785499', '1937785491'], 'December 10 2015', 2015)
          @apa1 = APA.new(@biblio1)
          @apa2 = APA.new(@revista1)
          @apa3 = APA.new(@biblio2)
@@ -29,17 +29,19 @@ describe APA do
            expect(@apa4.nomape).to eq("Autor, C. S.")
         end
         it "Ordenar" do
-            @lista1.insertarfinish(@biblio2)
-            @lista1.insertarfinish(@biblio1)
-            @lista1.sort
-            expect(@lista1.extraerstart).to eq(@biblio2)
-            expect(@lista1.extraerstart).to eq(@biblio1)
+            expect(@todos.insertar(@apa1)).to eq(true)
+            expect(@todos.insertar(@apa2)).to eq(true)
+            expect(@todos.insertar(@apa3)).to eq(true)
+            expect(@todos.insertar(@apa4)).to eq(true)
+            expect(@todos.insertar(@apa5)).to eq(true)
+            expect(@todos.ordenacion).to eq([@apa5,@apa4,@apa2,@apa3,@apa1])
+
         end
     end
     
     describe "comprobando las mayusculas" do
         it "mayuscula (la primera letra de cada palabra del título)" do
-            expect(@apa2.get_titulo).to eq('Programming Ruby')
+            expect(@apa6.get_titulo).to eq('Noticias')
         end
     end
 end
